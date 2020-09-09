99318d1647fd54c4bda53f1bbe5236576155b108
Santiago Alvarez <sao2207@gmail.com>
2020-09-09 10:50:23 -0500

    textContent and insertAdjacentText

--- 20 - The DOM/theDOMExcercise/the-dom.js
+++ 20 - The DOM/theDOMExcercise/the-dom.js
@@ -1,6 +1,17 @@
 const p = document.querySelectorAll('p');
 const items = document.querySelector('.items');
 const itemImag = items.querySelector('img');
-console.log(itemImag);
-console.log(items);
-console.log(p);
+const heading = document.querySelector('h2'); // the first one
+
+console.log(heading.innerHTML); // lo mismo que textContent
+console.log(heading.outerHTML); // Muestra toda la estructura
+console.dir(heading.textContent);
+// setting the textContent property on that element
+heading.textContent = 'Santi is cool';
+console.log(heading.textContent);
+
+// insertar texto with insertAdjacentText
+
+itemImag.insertAdjacentText('beforeBegin', 'hola');
+
+itemImag.insertAdjacentText('afterEnd', 'hola');
