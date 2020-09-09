3a492bd99e3c4016f9a83f18bbbfb4455e7079b2
Santiago Alvarez <sao2207@gmail.com>
2020-09-09 10:19:37 -0500

    seleccionando elementos querySelector - all

---
+++ 20 - The DOM/theDOMExcercise/the-dom.js
@@ -0,0 +1,6 @@
+const p = document.querySelectorAll('p');
+const items = document.querySelector('.items');
+const itemImag = items.querySelector('img');
+console.log(itemImag);
+console.log(items);
+console.log(p);
