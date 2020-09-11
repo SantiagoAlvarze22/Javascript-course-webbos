44ae59031c80ffb3c003b0ab8e00d0433d1a5655
Santiago Alvarez <sao2207@gmail.com>
2020-09-11 14:36:19 -0500

    e.target e.currentTarget eventBubbling eventPropagation

--- 29 - Events/events.js
+++ 29 - Events/events.js
@@ -18,11 +18,11 @@ buttonCool.addEventListener('click', hooray);
 butts.removeEventListener('click', handleClick);
 
 // Listen on multiple items
-const buyButtons = document.querySelectorAll('.buy');
+// const buyButtons = document.querySelectorAll('.buy');
 
-function buyItem() {
-  console.log('Buy Item');
-}
+// function buyItem() {
+//   console.log('Buy Item');
+// }
 
 // buyButtons.forEach(buyButton => {
 //   buyButton.addEventListener('click', buyItem);
@@ -30,8 +30,48 @@ function buyItem() {
 
 // another way is atteaching a function
 
-function attachBuyButtonClick(buyButtton) {
-  buyButtton.addEventListener('click', buyItem);
+// function attachBuyButtonClick(buyButtton) {
+//   buyButtton.addEventListener('click', buyItem);
+// }
+
+// buyButtons.forEach(attachBuyButtonClick);
+
+const buyButtons = document.querySelectorAll('.buy');
+
+function handleBuyButtonClick(event) {
+  console.log('you clicke a button');
+  const button = event.target;
+  //   console.log(event.target);
+  //   console.log(event.currentTarget);
+  //   console.log(event.target === event.currentTarget);
+  //   event.stopPropagation(); // Para evitar que el evento se vaya hacia los otros elementos que tienen listeners
+  //   console.log(button.textContent);
+  //   console.log('You are buying it!');
+  //   console.log(parseFloat(event.target.dataset.price));
 }
 
-buyButtons.forEach(attachBuyButtonClick);
+buyButtons.forEach(buyButton => {
+  buyButton.addEventListener('click', handleBuyButtonClick);
+});
+
+// eventlistener in Windows, to show the event bubbling
+
+// document.addEventListener('click', function() {
+//   console.log('You clicked the document');
+// });
+
+window.addEventListener(
+  'click',
+  function(event) {
+    console.log('You clicked the window');
+    console.log(event.target);
+  },
+  { capture: true }
+);
+
+const photoEl = document.querySelector('.photo');
+
+photoEl.addEventListener('mouseenter', function(e) {
+  console.log(e.cuurentTarget);
+  console.log(this);
+});
