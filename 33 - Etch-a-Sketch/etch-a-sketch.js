2ca336c5319ce488afa6f5422026780d75e2af97
Santiago Alvarez <sao2207@gmail.com>
2020-09-14 14:16:01 -0500

    setting colors hsl, swtich loop to move in the corren way the draw in the canvas

--- 33 - Etch-a-Sketch/etch-a-sketch.js
+++ 33 - Etch-a-Sketch/etch-a-sketch.js
@@ -2,6 +2,8 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 10;
+let hue = 0;
 
 // setup our canvas for drawning
 
@@ -10,22 +12,56 @@ const shakebutton = document.querySelector('.shake');
 const { width, height } = canvas;
 
 // create random x and y starting points on the canvas
-const x = Math.floor(Math.random() * width);
-const y = Math.floor(Math.random() * height);
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
-
+ctx.lineWidth = MOVE_AMOUNT;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.beginPath(); // start drawning
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
 // write a draw function
+function draw({ key }) {
+  // updating in every movement arrow the variable hue and change it the color automacally
+  hue += 1;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  console.log(key);
+  // start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // Move out x and y values depending on what the user sdid
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
 
 // write a handle for the keys
-
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
 // clear / shke function
 
 // listen for arrow keys
+window.addEventListener('keydown', handleKey);
