6fa3eff7b0bb29a9281b8190a89a10e83bef324b
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-09-27 20:10:33 -0500

    markAsCompleate function and delete item function

--- 57 - Shopping List/shopping.js
+++ 57 - Shopping List/shopping.js
@@ -2,7 +2,7 @@ const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
 
 // we need an array to hold out state
-const items = [];
+let items = [];
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -27,9 +27,15 @@ function displayItems() {
   const html = items
     .map(
       item => `<li class="shopping-item">
-        <input type="checkbox">
+        <input 
+            value="${item.id}" 
+            type="checkbox"
+            ${item.complete && 'checked'}
+        >
         <span class="itemName">${item.name}</span>
-        <button aria-label="remove ${item.name}">&times;</button>
+        <button aria-label="remove ${item.name}" value="${
+        item.id
+      }">&times;</button>
       </li>`
     )
     .join('');
@@ -38,10 +44,46 @@ function displayItems() {
 }
 
 function mirrorToLocalStorage() {
-  console.log('yas');
   localStorage.setItem('items', JSON.stringify(items));
+}
+
+// Show the items saved in the localStorage, evey time that the page load
+function restoreFromLocalStorage() {
+  const lsItems = JSON.parse(localStorage.getItem('items'));
+  if (lsItems.length) {
+    // items = lsItems;
+    // lsItems.forEach(item => items.push(item));
+    items.push(...lsItems);
+    list.dispatchEvent(new CustomEvent('itemsUpdated')); // allows to delete form the DOM the value
+  }
+}
+
+function deleteItem(id) {
+  console.log('delete', id);
+  // update our items array without this one
+  items = items.filter(item => item.id !== id);
+  console.log(items);
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+function markAsComplete(id) {
+  console.log('holi', id);
+  const itemRef = items.find(item => item.id === id);
+  itemRef.complete = !itemRef.complete;
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 shoppingForm.addEventListener('submit', handleSubmit);
 list.addEventListener('itemsUpdated', displayItems);
 list.addEventListener('itemsUpdated', mirrorToLocalStorage);
+// even delegation:  we listen or the clock on the list <UL> but then delegate the click over to the button if that is was clicked
+list.addEventListener('click', e => {
+  const id = parseInt(e.target.value);
+  if (e.target.matches('button')) {
+    deleteItem(id);
+  }
+  if (e.target.matches('input[type="checkbox"]')) {
+    markAsComplete(id);
+  }
+});
+restoreFromLocalStorage();
