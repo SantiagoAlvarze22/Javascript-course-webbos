645fba7902446642147d257457cb8321261d540d
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-09-28 10:02:53 -0500

    Changing the values of the modal

--- 58 - Gallery/gallery.js
+++ 58 - Gallery/gallery.js
@@ -7,6 +7,21 @@ function Gallery(gallery) {
   const modal = document.querySelector('.modal');
   const prevButton = modal.querySelector('.prev');
   const nextButton = modal.querySelector('.next');
+
+  function showImage(el) {
+    if (!el) {
+      console.info('no image to show');
+    }
+    // update the modal with this info
+    console.log(el);
+    modal.querySelector('img').src = el.src;
+    modal.querySelector('h2').textContent = el.title;
+    modal.querySelector('figure p').textContent = el.dataset.description;
+  }
+
+  images.forEach(image =>
+    image.addEventListener('click', e => showImage(e.currentTarget))
+  );
 }
 
 // use it on the page
