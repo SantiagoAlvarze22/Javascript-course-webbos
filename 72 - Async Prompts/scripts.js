946801ccfa04553ff85b202233628ee68c789aea
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-02 17:18:52 -0500

    function create the popup, btn cancel

--- 72 - Async Prompts/scripts.js
+++ 72 - Async Prompts/scripts.js
@@ -1,13 +1,17 @@
+function wait(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 function ask(options) {
-  return new Promise(function(resolve) {
+  return new Promise(async function(resolve) {
     // first we need to create a popup with all the fields in it
     const popup = document.createElement('form');
     popup.classList.add('popup');
     popup.insertAdjacentHTML(
       'afterbegin',
-      `
-        <fieldset>
+      `<fieldset>
             <label>${options.title}</label>
+            <input type="text" name="input" />
+            <button type="submit">Submit</button>
         </fieldset>
     `
     );
@@ -16,9 +20,27 @@ function ask(options) {
       const skipButton = document.createElement('button');
       skipButton.type = 'button';
       skipButton.textContent = 'Cancel';
-      // TODO: LISTES FOR A CLICK ON THAT CANCEL BUTTON
+      popup.firstElementChild.appendChild(skipButton);
+      // TODO: LISTEN FOR A CLICK ON THAT CANCEL BUTTON
     }
     // listen for the submit event on the inputs
+    popup.addEventListener(
+      'submit',
+      function(e) {
+        e.preventDefault();
+        resolve(e.target.input.value);
+        // remove the addeventlistener once is run {once:true}
+
+        // remove it from the DOM entirely
+      },
+      { once: true }
+    );
     // when someone does submit it, resolve the data that was in the input box!!
+
+    // insert that popup into the DOM
+    document.body.appendChild(popup);
+    // put a vary small timeout before we add the open class
+    await wait(50);
+    popup.classList.add('open');
   });
 }
