da077818cd33b7e5f7d95c5f3a98e40848146950
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-04 16:44:13 -0500

    function that allows show the text with specific amout of time

--- 73 - Async Typer/scripts.js
+++ 73 - Async Typer/scripts.js
@@ -5,3 +5,18 @@ function wait(ms = 0) {
 function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
+async function draw(el) {
+  const text = el.textContent;
+  let soFar = '';
+  for (const letter of text) {
+    soFar += letter;
+    // adding the new content to the DOM
+    el.textContent = soFar;
+    // Wait for amount of time
+    const { typeMin, typeMax } = el.dataset;
+    const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+    await wait(amountOfTimeToWait);
+  }
+}
+
+document.querySelectorAll('[data-type]').forEach(draw);
