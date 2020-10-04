53b2ec37143cacf433efd667ce3cf702e3067776
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-04 17:12:39 -0500

    drawing the text with a recurssion function

--- 73 - Async Typer/scripts.js
+++ 73 - Async Typer/scripts.js
@@ -5,18 +5,38 @@ function wait(ms = 0) {
 function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
-async function draw(el) {
-  const text = el.textContent;
-  let soFar = '';
-  for (const letter of text) {
-    soFar += letter;
-    // adding the new content to the DOM
-    el.textContent = soFar;
-    // Wait for amount of time
-    const { typeMin, typeMax } = el.dataset;
-    const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
-    await wait(amountOfTimeToWait);
-  }
-}
+// async function draw(el) {
+//   const text = el.textContent;
+//   let soFar = '';
+//   for (const letter of text) {
+//     soFar += letter;
+//     // adding the new content to the DOM
+//     el.textContent = soFar;
+//     // Wait for amount of time
+//     const { typeMin, typeMax } = el.dataset;
+//     const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+//     await wait(amountOfTimeToWait);
+//   }
+// }
 
+// recurssion
+function draw(el) {
+  let index = 1;
+  const text = el.textContent;
+  const { typeMin, typeMax } = el.dataset;
+
+  async function drawLetter() {
+    el.textContent = text.slice(0, index);
+    index += 1;
+    const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+    // exit condition
+    await wait(amountOfTimeToWait);
+    if (index <= text.length) {
+      drawLetter();
+    }
+  }
+
+  // when this function draw runs, kick off drawLetter
+  drawLetter();
+}
 document.querySelectorAll('[data-type]').forEach(draw);
