45bf88a3ac21969bcfcdcc7a5190d09e8aa773fc
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-04 22:06:06 -0500

    fetch data and return the joke by clicking the button

---
+++ 76 - Dad Jokes/jokes.js
@@ -0,0 +1,31 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
+const buttonText = [
+  'Ugh.',
+  '🤦🏻‍♂️',
+  'omg dad.',
+  'you are the worst',
+  'seriously',
+  'stop it.',
+  'please stop',
+  'that was the worst one',
+];
+
+async function fetchJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+async function handleClick() {
+  // joke is returned by fetchJoke function in data
+  const { joke } = await fetchJoke();
+  console.log(joke);
+}
+
+jokeButton.addEventListener('click', handleClick);
