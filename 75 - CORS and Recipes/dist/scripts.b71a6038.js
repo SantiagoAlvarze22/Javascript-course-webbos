200077db1eb390c655ed6353c9de6372b51118e6
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-04 20:16:48 -0500

    fetching tthe data and using proxy to avoid CORS policy issue

--- 75 - CORS and Recipes/dist/scripts.b71a6038.js
+++ 75 - CORS and Recipes/dist/scripts.b71a6038.js
@@ -118,7 +118,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"scripts.js":[function(require,module,exports) {
+const baseEndpoint = 'http://www.recipepuppy.com/api';
+const proxy = `https://cors-anywhere.herokuapp.com/`;
 
+async function fetchRecipes(query) {
+  const res = await fetch(`${proxy}${baseEndpoint}/?q=${query}`);
+  const data = await res.json();
+  console.log(data);
+}
+
+fetchRecipes('pizza');
 },{}],"../../../Users/santi/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57784" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58540" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
