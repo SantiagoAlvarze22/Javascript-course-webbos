1cd9a729d6bcb2d7c5df8cad67c0061ec3ff0e3f
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-05 10:28:36 -0500

    passing input to show in the DOM

--- 77 - Currency Converter/money.js
+++ 77 - Currency Converter/money.js
@@ -1,5 +1,8 @@
 const fromSelect = document.querySelector('[name="from_currency"]');
 const toSelect = document.querySelector('[name="to_currency"]');
+const fromInput = document.querySelector('[name="from_amount"]');
+const toEl = document.querySelector('.to_amount');
+const form = document.querySelector('.app form');
 const endpoint = 'https://api.exchangeratesapi.io/latest';
 const ratesByBase = {}; // store all of the rates in it
 
@@ -66,9 +69,18 @@ async function convert(amount, from, to) {
   }
   // convert that amout that they passed it
   const rate = ratesByBase[from].rates[to];
-  console.log(rate);
   const convertedAmount = rate * amount;
   console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+}
+
+async function handleInput(e) {
+  const rawAmount = await convert(
+    fromInput.value,
+    fromSelect.value,
+    toSelect.value
+  );
+  console.log(rawAmount);
+  // toEl.textContent = rawAmount;
 }
 
 const optionsHTML = generateOptions(currencies);
@@ -76,3 +88,5 @@ const optionsHTML = generateOptions(currencies);
 // populate the options elements
 fromSelect.innerHTML = optionsHTML;
 toSelect.innerHTML = optionsHTML;
+
+form.addEventListener('input', handleInput);
