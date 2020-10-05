69a9728c5fc2b71e6f30d4fc075ed4134944ffe2
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-05 09:18:30 -0500

    adding the data

--- 77 - Currency Converter/money.js
+++ 77 - Currency Converter/money.js
@@ -1,3 +1,6 @@
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -44,3 +47,7 @@ function generateOptions(options) {
 
 const optionsHTML = generateOptions(currencies);
 console.log(optionsHTML);
+
+// populate the options elements
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
